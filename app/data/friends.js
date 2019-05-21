eb7887fe26aaa2131599bd48a52733d0f60a98ca
Armando Morales <amorales944@gmail.com>
2019-05-21 04:25:36 -0500

    Created html and survey pages that will be used in the app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,25 @@
+
+var selectedAnswers = [];
+var questions = document.getElementById("questions");
+
+function questionSelected ()  {
+  for (var i = 0; i < selectedAnswers.length; i++) {     // parsing with array.length
+    console.log(selectedAnswers[i]);
+}
+
+}
+
+
+
+
+
+
+
+
+
 const people = [
 {
+    "routeName":  "ahmed",
     "name":"Ahmed",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
