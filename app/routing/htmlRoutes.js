c4f7254d411220ccda5c1b586f222c107ce7163d
Armando Morales <amorales944@gmail.com>
2019-05-22 03:24:16 -0500

    Corrected file path to home.html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,12 +7,12 @@ const path = require("path");
 module.exports = function (app) {
 app.use("/home", function(req, res) {
     // res.send("Welcome to friend finder home")
-    res.sendFile(path.join(__dirname, "../public/home.html"));             app/public/home.html
+    res.sendFile(path.join(__dirname, "/home.html"));
   });
   
   app.get("/survey", function(req, res) {
     // res.send("Welcome to friend finder survey")
-    res.sendFile(path.join(__dirname, "/survey.html"));             app/public/survey.html
+    res.sendFile(path.join(__dirname, "/survey.html"));
   });
 }
 
